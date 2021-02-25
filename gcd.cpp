3dc2576c3145d72a51c1d925a832124307bc372f
Md Noman Khan <54404474+noman2002@users.noreply.github.com>
2021-02-25 22:38:42 +0530

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int gcd (int m, int n)
+{
+    if(n == 0) return m;
+    cout << "gcd(" << m << ',' << n <<')'<< endl; 
+    return gcd(n, m % n);
+}
+
+int main()
+{
+  int x,y;
+  cin>>x>>y;
+    cout << "gcd = " << gcd(x,y) << endl;
+    return 0;
+}
