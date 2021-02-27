1a13b93dad88cfc07d48c492bb940bdf53e37e98
Md Noman Khan <54404474+noman2002@users.noreply.github.com>
2021-02-27 22:00:13 +0530

    Create pointers_and_arrays.cpp

---
+++ pointers_and_arrays.cpp
@@ -0,0 +1,32 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float arr[3];
+
+ 
+    float *ptr;
+    
+    cout << "Displaying address using arrays: " << endl;
+
+    // use for loop to print addresses of all array elements
+    for (int i = 0; i < 3; ++i)
+    {
+        cout << "&arr[" << i << "] = " << &arr[i] << endl;
+    }
+
+    ptr = arr;
+
+    cout<<"\nDisplaying address using pointers: "<< endl;
+
+    
+    for (int i = 0; i < 3; ++i)
+    {
+        cout << "ptr + " << i << " = "<< ptr + i << endl;
+    }
+
+    return 0;
+}
